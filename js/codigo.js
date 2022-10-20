f0d29bf2ee5b5c46cc164257e5c88350a0fe419b
FelipeWallace <felipewallacepc@gmail.com>
2022-10-20 17:28:40 -0300

    Exercício Finalizado

--- js/codigo.js
+++ js/codigo.js
@@ -4,4 +4,38 @@ function calculaIMC () {
   let IMC = (peso/(altura ** 2));
   IMC = IMC.toFixed(1);
   document.getElementById("resp").innerHTML = "IMC: " + IMC;
+  destaca(IMC);
+}
+
+function destaca(IMC) {
+  if (IMC <= 18.5) {
+    const destaque = document.getElementById("1");
+    destaque.classList.add("resultado");
+    destaque.innerHTML = "Abaixo do peso "+"<i class='fa-solid fa-mosquito'></i>"
+  }
+  else if (IMC > 18.5 && IMC <= 24.9) {
+    const destaque = document.getElementById("2");
+    destaque.classList.add("resultado");
+    destaque.innerHTML = "Peso ideal " + "<i class='fa-solid fa-fish'></i>"
+  }
+  else if (IMC > 24.9 && IMC <= 29.9) {
+    const destaque = document.getElementById("3");
+    destaque.classList.add("resultado");
+    destaque.innerHTML = "Levemente acima do peso " + "<i class='fa-solid fa-crow'></i>"
+  }
+  else if (IMC > 29.9 && IMC <= 34.9) {
+    const destaque = document.getElementById("4");
+    destaque.classList.add("resultado");
+    destaque.innerHTML = "Obesidade grau I " + "<i class='fa-solid fa-otter'></i>"
+  }
+  else if (IMC > 34.9 && IMC <= 39.9) {
+    const destaque = document.getElementById("5");
+    destaque.classList.add("resultado");
+    destaque.innerHTML = "Obesidade grau II " + "<i class='fa-solid fa-cow'></i>"
+  }
+  else {
+    const destaque = document.getElementById("6");
+    destaque.classList.add("resultado");
+    destaque.innerHTML = "Obesidade grau III " + "<i class='fa-solid fa-hippo'></i>"
+  }
 }
