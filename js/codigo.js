972ccf7e8718846466ad02814a93cd58f9c57afa
Felipe Wallace <felipewallacepc@gmail.com>
2024-01-10 09:40:12 -0300

    Update codigo.js
    Adicionei uma função para limpar os resultados depois de 3 segundos

--- js/codigo.js
+++ js/codigo.js
@@ -12,30 +12,46 @@ function destaca(IMC) {
     const destaque = document.getElementById("1");
     destaque.classList.add("resultado");
     destaque.innerHTML = "Abaixo do peso "+"<i class='fa-solid fa-mosquito'></i>"
+    limparResultado(destaque);
   }
   else if (IMC > 18.5 && IMC <= 24.9) {
     const destaque = document.getElementById("2");
     destaque.classList.add("resultado");
     destaque.innerHTML = "Peso ideal " + "<i class='fa-solid fa-fish'></i>"
+    limparResultado(destaque);
   }
   else if (IMC > 24.9 && IMC <= 29.9) {
     const destaque = document.getElementById("3");
     destaque.classList.add("resultado");
     destaque.innerHTML = "Levemente acima do peso " + "<i class='fa-solid fa-crow'></i>"
+    limparResultado(destaque);
   }
   else if (IMC > 29.9 && IMC <= 34.9) {
     const destaque = document.getElementById("4");
     destaque.classList.add("resultado");
     destaque.innerHTML = "Obesidade grau I " + "<i class='fa-solid fa-otter'></i>"
+    limparResultado(destaque);
   }
   else if (IMC > 34.9 && IMC <= 39.9) {
     const destaque = document.getElementById("5");
     destaque.classList.add("resultado");
     destaque.innerHTML = "Obesidade grau II " + "<i class='fa-solid fa-cow'></i>"
+    limparResultado(destaque);
   }
   else {
     const destaque = document.getElementById("6");
     destaque.classList.add("resultado");
     destaque.innerHTML = "Obesidade grau III " + "<i class='fa-solid fa-hippo'></i>"
+    limparResultado(destaque);
   }
+}
+
+function limparResultado(destaque)
+{
+  setTimeout(
+    ()=>{
+      destaque.classList.remove("resultado");
+    },
+    3000
+  )
 }
